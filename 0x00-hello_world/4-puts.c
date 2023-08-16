bc657c8e5c848c7f9aa0053b1d367a1947d3b819
kimo234 <ojda05887@gmail.com>
2023-08-16 04:17:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,5 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-	 { 
-		 puts("\"Programming is like building a multilingual puzzle");
-                 retutn(0)
-	 }	 
-
+{puts("\"Programming is like building a multilingual puzzle");
+retutn(0) ; }
